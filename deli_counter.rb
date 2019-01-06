23021cca95f47cd232878f4101e993ff16006795
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 01:45:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,5 +11,5 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome #{name}, you are number #{katz_deli.index(name) +1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.index(name) +1} in line."
 end
