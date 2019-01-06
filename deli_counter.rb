ec183ec632ee4cb86b8dfba8f176f714a694eb24
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 01:21:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
   customers = ""
-  katz_deli.map.with_index{|person, i| }
+  katz_deli.map.with_index{|person, i| customers.concat("#{i}. #{person}") }
   puts "The line is currently: #{}"
   end
 end
