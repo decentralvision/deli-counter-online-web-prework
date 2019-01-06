3d34f2d9f352f85a5c5fa2ceecc9a0f259a3f4e0
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-06 01:47:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,3 +13,11 @@ def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.index(name) +1} in line."
 end
+
+def now_serving
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  end
+  katz_deli[0]
+  katz_deli.shift
+end
